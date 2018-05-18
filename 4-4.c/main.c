b3fd9344ed2b8fa885ab0e4d454072dfadafa0b5
lishiyao-ucb <1281484931@qq.com>
2018-05-18 16:08:26 +0800

    add 4-5.c

--- 4-4.c/main.c
+++ 4-4.c/main.c
@@ -10,7 +10,7 @@ int main()
             words++;
         else if(a==' ')
             space++;
-        else if(a>=0||a<=9)
+        else if(a>='0'&&a<='9')
             numbers++;
         else others++;
     }
