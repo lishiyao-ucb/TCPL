70a4c42f7e00a4e1c0fdafa682b56969a434b34d
lishiyao-ucb <1281484931@qq.com>
2018-05-19 14:32:17 +0800

    add 6-1.c

---
+++ 6-1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+#define N 10
+int main()
+{
+    int a[N],i,j;
+    for(i=0; i<N; i++)
+        a[i]=i+1;
+    for(i=2; i<N; i++){
+        for(j=2; j<=sqrt(a[i]); j++)
+        {
+            if(a[i]%j==0)
+                a[i]=0;
+        }
+    }
+    printf("the primers are:\n");
+    for(i=0; i<N; i++)
+    {
+        if(a[i]!=0)
+            printf("%d ",a[i]);
+    }
+
+    return 0;
+}
