e6f63321b6677fef1ada671076dbdb271bbf62fe
lishiyao-ucb <1281484931@qq.com>
2018-05-19 11:38:28 +0800

    add example6.8.c

---
+++ example6.8/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char string[81];
+    int i,num=0,word=0;
+    printf("input a sentence:\n");
+    gets(string);
+    for(i=0;string[i]!='\0';i++)
+    {
+        if(string[i]==' ') word=0;
+        else if(word==0)
+        {
+            word=1;
+            num++;
+        }
+    }
+    printf("There are %d words in the sentence.\n",num);
+    return 0;
+}
